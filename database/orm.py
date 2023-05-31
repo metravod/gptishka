168a5279c49082c6a6020dfdeeed3fc06ec1051b
metravod <arttrek42@gmail.com>
2023-05-31 16:14:04 +0300

    clean up orm, fix save context

--- database/orm.py
+++ database/orm.py
@@ -27,50 +27,15 @@ def get_user(tg_id) -> User | None:
     return session.query(User).filter(User.tg_id == tg_id).first()
 
 
-def add_context(tg_id, name: str, context_id: int, is_active: bool):
-    session = _create_session()
-    user = get_user(tg_id)
-    context = get_context(tg_id, name)
-    if context is None:
-        new_context = UserContext(context_id=context_id, owner=user.id, name=name, is_active=is_active)
-        session.add(new_context)
-        session.commit()
-
-
 def get_context(tg_id, name: str) -> UserContext | None:
     session = _create_session()
     user = get_user(tg_id)
     return session.query(UserContext).filter(UserContext.owner == user.id, UserContext.name == name).first()
 
 
-def get_active_context(tg_id) -> UserContext | None:
-    session = _create_session()
-    user = get_user(tg_id)
-    return session.query(UserContext).filter(UserContext.owner == user.id, UserContext.is_active).first()
-
-
-def disactive_context(tg_id, name: str):
-    session = _create_session()
-    user = get_user(tg_id)
-    session.query(UserContext).filter(UserContext.owner == user.id, UserContext.name == name).update({'is_active': False})
-    session.commit()
-
-
-def get_talk(tg_id, name_context) -> list:
-    context = get_context(tg_id, name_context)
-    return [el.message for el in context.messages]
-
-
 def get_list_contexts_by_user(tg_id) -> list:
     user = get_user(tg_id)
     return [context.name for context in user.contexts]
-
-
-def update_count_tokens_in_context(tg_id, name, count_tokens: int):
-    session = _create_session()
-    user = get_user(tg_id)
-    session.query(UserContext).filter(UserContext.owner == user.id, UserContext.name == name).update({'tokens': count_tokens})
-    session.commit()
 
 
 def delete_user_context(tg_id, name_context):
@@ -87,6 +52,11 @@ def delete_user_context(tg_id, name_context):
 def save_context(tg_id, name, content):
     session = _create_session()
     user = get_user(tg_id)
-    new_context = UserContext(owner=user.id, name=name, content=content)
-    session.add(new_context)
-    session.commit()
+    context = get_context(user.id, name)
+    if context:
+        context.update({'content': content})
+        session.commit()
+    else:
+        new_context = UserContext(owner=user.id, name=name, content=content)
+        session.add(new_context)
+        session.commit()
