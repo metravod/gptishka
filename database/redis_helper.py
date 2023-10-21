c15dda64065715444b275f87ca9e2165f8a216da
Artem Lyadov <arttrek42@gmail.com>
2023-10-21 19:33:44 +0300

    migration to aiogram 3 (#8)
    refactor bot for migration on aiмogram 3.x

--- database/redis_helper.py
+++ database/redis_helper.py
@@ -3,7 +3,7 @@ import json
 import redis
 
 redis_host = os.getenv('REDIS_HOST')
-redis_port = os.getenv('REDIS_PORT')
+redis_port = int(os.getenv('REDIS_PORT'))
 redis_password = os.getenv('REDIS_PASSWORD')
 
 
